fd564482743670dfa43c525234f40744a535eb23
lxvs <jn.apsd@gmail.com>
2022-08-15 13:05:07 +0800

    Cherry-pick uninstallation script from jsa

--- uninstall.bat
+++ uninstall.bat
@@ -1,58 +1,91 @@
+@REM Just Archive It Uninstallation
+@REM https://github.com/lxvs/jai
+
 @echo off
 setlocal
-set silent=
-if "%~1" == "--silent" (set silent=1)
-title Just Archive It Uninstallation
-set version=
-set target_dir=
-set item_amount=
-set filetype=
-set UserPath=
-set "regPathSoftware=HKCU\Software\lxvs\jai"
-call:GetReg "%regPathSoftware%" "version" version
-call:GetReg "%regPathSoftware%" "target" target_dir
-if not defined version if not defined target_dir if not defined silent (
-    >&2 echo warning: no installation found
-    set silent=1
-)
-if not defined silent (
-    @echo Uninstalling JAI %version% installed at `%target_dir%'
-)
 
-:start
-call:GetReg "HKCU\Environment" "Path" UserPath
+set name=jsa
+set regpath=HKCU\Software\lxvs\jai
+set silent=
+set term=
+set install=
+set exitcode=0
+:parseargs
+if %1. == . (goto endparseargs)
+set term=1
+if /i "%~1" == "--silent" (
+    set silent=1
+    shift /1
+    goto parseargs
+)
+if /i "%~1" == "--install" (
+    set install=1
+    shift /1
+    goto parseargs
+)
+if "%~1" == "/?" (goto help)
+if "%~1" == "-?" (goto help)
+if /i "%~1" == "-h" (goto help)
+if /i "%~1" == "--help" (goto help)
+>&2 echo error: invalid argument `%~1'
+>&2 echo Try `uninstall.bat --help' for more information.
+exit /b 1
+:endparseargs
+
+if defined install (
+    if not defined silent (
+        call %~dp0install.bat
+    ) else (
+        call %~dp0install.bat --silent
+    )
+    exit /b
+)
+call:getreg "HKCU\Environment" "Path" UserPath
+call:getreg "%regpath%" "path" installation
+call:getreg "%regpath%" "amount" item_amount
+call:getreg "%regpath%" "filetype" filetype
 setlocal EnableDelayedExpansion
 if defined UserPath (
-    if defined target_dir (
-        if defined silent (
-            setx Path "!UserPath:%target_dir%;=!" 1>nul 2>&1
+    if not defined silent (
+        if defined installation (
+            setx Path "!UserPath:%installation%;=!" 1>nul
         ) else (
-            setx Path "!UserPath:%target_dir%;=!" 1>nul
+            >&2 echo warning: no installation found; try to uninstall anyway
         )
+        reg delete "%regpath%" /f 1>nul
+        reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\%name%.exe" /f 1>nul
+    ) else (
+        if defined installation (setx Path "!UserPath:%installation%;=!" 1>nul 2>&1)
+        reg delete "%regpath%" /f 1>nul 2>&1
+        reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\%name%.exe" /f 1>nul 2>&1
     )
+) else (
+    if not defined silent (
+        >&2 echo error: failed to get user Path
+    )
+    set exitcode=1
+    goto end
 )
 endlocal
 
-if exist "%target_dir%" (
+if exist "%installation%" (
     1>nul 2>&1 (
-        del "%target_dir%\jai.bat"
-        del "%target_dir%\7za.exe"
-        del "%target_dir%\7za.dll"
-        del "%target_dir%\License.txt"
-        del "%target_dir%\License-7z.txt"
-        rmdir "%target_dir%"
+        del "%installation%\jai.bat"
+        del "%installation%\7za.exe"
+        del "%installation%\7za.dll"
+        del "%installation%\License.txt"
+        del "%installation%\License-7z.txt"
+        rmdir "%installation%"
     )
 )
 
-call:GetReg "%regPathSoftware%" "amount" item_amount
-call:GetReg "%regPathSoftware%" "filetype" filetype
 set /a "item_amount=item_amount"
 set "regPathDir=HKCU\Software\Classes\Directory\shell"
 set "regPathDirBg=HKCU\Software\Classes\Directory\Background\shell"
 set "regPathFileType=HKCU\Software\Classes\SystemFileAssociations\.$$FileType$$\shell"
 setlocal enableDelayedExpansion
 if %item_amount% GTR 0 (
-    for /L %%i in (1,1,%item_amount%) do (
+    for /l %%i in (1,1,%item_amount%) do (
         reg delete "%regPathDir%\jai_%%i" /f 1>nul 2>&1
         reg delete "%regPathDirBg%\jai_%%i" /f 1>nul 2>&1
         for %%j in (%filetype%) do (
@@ -61,25 +94,43 @@ if %item_amount% GTR 0 (
     )
 )
 endlocal
-if defined silent (
-    reg delete "%regPathSoftware%" /f 1>nul 2>&1
+
+if not defined silent (echo Uninstall complete.)
+goto end
+
+:getreg
+set %3=
+set getregretval=
+if /i "%~2" == "/ve" (
+    set getreg_switch=/ve
+    set getreg_key=
+    set "getreg_name=(Default)"
 ) else (
-    reg delete "%regPathSoftware%" /f 1>nul
-    @echo Complete.
-    pause
+    set getreg_switch=/v
+    set getreg_key="%~2"
+    set "getreg_name=%~2"
+)
+for /f "skip=2 tokens=1* delims=" %%a in ('reg query "%~1" %getreg_switch% %getreg_key% 2^>nul') do (
+    call:getregparse "%%~a"
+)
+if defined getregretval (set "%3=%getregretval%")
+exit /b
+
+:getregparse
+if "%~1" == "" (exit /b 1)
+set "getregparse_str=%~1"
+set "getregparse_str=%getregparse_str:    =	%
+for /f "tokens=1,2* delims=	" %%A in ("%getregparse_str%") do (
+    if /i "%getreg_name%" == "%%~A" (set "getregretval=%%~C")
 )
 exit /b
 
-:GetReg
-@REM Path "name"|/ve &variable
-if "%~2" == "/ve" (
-    set "GetReg_switch=/ve"
-    set "GetReg_key="
-    set "GetReg_name=(default)"
-) else (
-    set "GetReg_switch=/v"
-    set "GetReg_key=%~2"
-    set "GetReg_name=%~2"
-)
-for /f "skip=2 tokens=1,2*" %%a in ('reg query "%~1" %GetReg_switch% %GetReg_key% 2^>NUL') do if /i "%%~a" == "%GetReg_name%" set "%3=%%c"
-exit /b
+:help
+echo usage: uninstall.bat
+echo    or: uninstall.bat --silent
+echo    or: uninstall.bat --install
+exit /b 0
+
+:end
+if not defined term (pause)
+exit /b %exitcode%
